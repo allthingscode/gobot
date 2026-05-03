b85371f3d151aaa34a5dc69b8f8b504b083fabe0
Matthew <Matthew@AllThingsCode.com>
2026-05-02 21:31:06 -0500

    test(doctor): fix unguarded Unix path in checkBrowser test [C-186]

--- internal/doctor/doctor_test.go
+++ internal/doctor/doctor_test.go
@@ -563,7 +563,7 @@ func TestCheckBrowser_Found(t *testing.T) {
 	t.Parallel()
 
 	mockLookPath := func(name string) (string, error) {
-		return "/usr/bin/" + name, nil
+		return filepath.Join("/usr/bin", name), nil
 	}
 
 	r := checkBrowser(mockLookPath)
